009fa0ac5ac2bd10300c114793bcf70d0f1c5a54
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-19 17:35:40 +0200

    create doctor model

---
+++ lib/features/home/data/models/doctor_model.dart
@@ -0,0 +1,19 @@
+class DoctorModel {
+  final String name;
+  final String specialty;
+  final double rating;
+  final int reviews;
+  final String fee;
+  final String imageAsset;
+  final bool isFavorite;
+
+  const DoctorModel({
+    required this.name,
+    required this.specialty,
+    required this.rating,
+    required this.reviews,
+    required this.fee,
+    required this.imageAsset,
+    this.isFavorite = false,
+  });
+}
