809ded79b89217e68929cc6945158c58a8b0ffaf
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-19 17:27:46 +0200

    create onboarding view model

---
+++ lib/features/on_boarding_view/data/models/on_boarding_view_model.dart
@@ -0,0 +1,11 @@
+class OnBoardingViewModel {
+  final String title;
+  final String description;
+  final String image;
+
+  OnBoardingViewModel({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
