effae80b8e70f2180dc977b83de332c1eea52efd
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-19 17:20:39 +0200

    welcome to doctor appointment app

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:doctor_appointment/core/utils/app_theme.dart';
+import 'package:doctor_appointment/core/utils/go_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const DoctorAppointment(),
+    ),
+  );
+}
+
+class DoctorAppointment extends StatelessWidget {
+  const DoctorAppointment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          theme: AppTheme.theme,
+          // ignore: deprecated_member_use
+          useInheritedMediaQuery: true,
+          builder: DevicePreview.appBuilder,
+          locale: DevicePreview.locale(context),
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
